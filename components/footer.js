f3d33a317ed9936764e992196d02b6345343dd85
SavageCabbagee <92659267+SavageCabbagee@users.noreply.github.com>
2022-09-21 23:23:24 +0800

    sticky footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Footer() {
     return (
-        <div className='flex bottom-0 bg-slate-900 p-3 w-full items-center'>
+        <div className='sticky flex bottom-0 bg-slate-900 p-3 w-full items-center'>
             <p className='text-xs text-white'>Built with Next.js and Tailwind CSS</p>
             <div className='inline-flex text-xs ml-auto'>               
                 <a href = 'https://twitter.com/Savageecabbage' target="_blank" rel="noopener noreferrer" className='p-1 hover:bg-neutral-700 rounded-full items-center'>
